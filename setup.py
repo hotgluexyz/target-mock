edd25c721d1fee4be6b4963d4bc2030279c757eb
breno-sapucaia <brenoss.magalhaes@gmail.com>
2026-07-17 10:34:49 -0300

    bumping python version classifiers

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14"
     ],
     keywords=["ELT", "Singer", "Target", "Mock"],
     packages=find_packages(),
@@ -41,5 +41,5 @@ setup(
             "target-mock=target_mock.target:TargetMock.cli",
         ],
     },
-    python_requires=">=3.7",
+    python_requires=">=3.10",
 ) 
